2aaf42b889dd1ac2d5083f4e2a3270302c6e5b88
Guilherme Samuel <guilhermesamuel79@gmail.com>
2021-07-29 02:26:37 -0300

    feat: defined custom errors

--- firecannon/errors.py
+++ firecannon/errors.py
@@ -1,2 +1,15 @@
-class ProblemTypeNotSupplied:
-  pass
+class ProblemTypeNotSuppliedException(Exception):
+    """thrown when problem type (classification or regression) is not supplied"""
+    def __str__(self):
+        return f"pass 'classification' or 'regression' via test settings"
+
+
+class IncompatibleDataShapeException(Exception):
+    """thrown when shapes of X and y are incompatiple"""
+
+    def __init__(self, X_rows: int, y_rows: int):
+        self.message = f'X has {X_rows} rows, but y has {y_rows} rows'
+        super(Exception, self).__init__(self.message)
+
+    def __str__(self):
+        return self.message
