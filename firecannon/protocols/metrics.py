d8d41d9ead5c1e7b187d83f7da743769c7352974
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-10-19 14:51:25 -0300

    feat: define protocols to metrics

--- firecannon/protocols/metrics.py
+++ firecannon/protocols/metrics.py
@@ -1,25 +1,15 @@
-from sklearn.metrics import accuracy_score, recall_score, precision_score, mean_squared_error, r2_score
+class Metrics:
+    def mse(self, y_true, y_pred):
+        raise NotImplementedError('mse')
 
+    def r2(self, y_true, y_pred):
+        raise NotImplementedError('r2')
 
-class RegressionMetrics:
-    @staticmethod
-    def mse(y_true, y_pred):
-        return mean_squared_error(y_true, y_pred)
+    def accuracy(self, y_true, y_pred):
+        raise NotImplementedError('accuracy')
 
-    @staticmethod
-    def r2_score(y_true, y_pred):
-        return r2_score(y_true, y_pred)
+    def precision(self, y_true, y_pred):
+        raise NotImplementedError('precision')
 
-
-class ClassificationMetrics:
-    @staticmethod
-    def accuracy_score(y_true, y_pred):
-        return accuracy_score(y_true, y_pred)
-
-    @staticmethod
-    def precision_score(y_true, y_pred):
-        return precision_score(y_true, y_pred)
-
-    @staticmethod
-    def recall_score(y_true, y_pred):
-        return recall_score(y_true, y_pred)
+    def recall(self, y_true, y_pred):
+        raise NotImplementedError('recall')
