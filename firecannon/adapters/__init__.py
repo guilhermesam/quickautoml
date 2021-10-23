3681522dfee0c5d0b1895aaa7552b1b5137f8db2
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-10-23 19:03:40 -0300

    refact: move directory code to py modules

---
+++ firecannon/adapters/__init__.py
@@ -0,0 +1,1 @@
+from . import sklearn_models_adapters
--- firecannon/adapters/metrics_adapters.py
+++ firecannon/adapters/metrics_adapters.py
@@ -1,19 +1,24 @@
-from firecannon.protocols.metrics import Metrics
+from firecannon.protocols import Metrics
 from sklearn.metrics import accuracy_score, recall_score, precision_score, mean_squared_error, r2_score
 
 
 class SKLearnMetrics(Metrics):
-    def mse(self, y_true, y_pred):
+    @staticmethod
+    def mse(y_true, y_pred):
         return mean_squared_error(y_true, y_pred)
 
-    def r2(self, y_true, y_pred):
+    @staticmethod
+    def r2(y_true, y_pred):
         return r2_score(y_true, y_pred)
 
-    def accuracy(self, y_true, y_pred):
+    @staticmethod
+    def accuracy(y_true, y_pred):
         return accuracy_score(y_true, y_pred)
 
-    def precision(self, y_true, y_pred):
+    @staticmethod
+    def precision(y_true, y_pred):
         return precision_score(y_true, y_pred)
 
-    def recall(self, y_true, y_pred):
+    @staticmethod
+    def recall(y_true, y_pred):
         return recall_score(y_true, y_pred)
