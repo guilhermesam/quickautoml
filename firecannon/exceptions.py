7041b4a1a2f5f200cd5d2d497e02ea6952702237
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-11-02 20:03:51 -0300

    refact: remove unnecessary comments

--- firecannon/exceptions.py
+++ firecannon/exceptions.py
@@ -1,5 +1,4 @@
 class IncompatibleDataShapeException(Exception):
-    """thrown when shapes of X and y are incompatiple"""
     def __init__(self, X_rows: int, y_rows: int):
         self.message = f'X has {X_rows} rows, but y has {y_rows} rows'
         super(Exception, self).__init__(self.message)
@@ -8,7 +7,6 @@ class IncompatibleDataShapeException(Exception):
         return self.message
 
 class InvalidParamException(Exception):
-    """thrown when supplied model is not a sklearn estimator implementation"""
     def __init__(self, message: str) -> None:
         super().__init__(message)
         self.message = message
