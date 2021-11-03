feb4999bea19b42cbc429125737e32a117f03954
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-11-03 09:28:25 -0300

    refact: applied capital letters pattern

--- firecannon/utils.py
+++ firecannon/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from json import dump
 from numpy import ndarray, array
 
-from firecannon.exceptions import InvalidParamException
+from firecannon.exceptions import IncompatibleDataShapeException
 
 
 def write_json(data: dict, filepath: str) -> None:
@@ -12,14 +12,13 @@ def write_json(data: dict, filepath: str) -> None:
     except IOError as error:
         raise error(f'I/O Error: {error}')
 
-
-def check_shape_compatibility(x: any, y: any) -> bool:
-    x = convert_to_np_array(x)
+def check_shape_compatibility(X: any, y: any) -> bool:
+    X = convert_to_np_array(X)
     y = convert_to_np_array(y)
 
     ROW_INDEX = 0
-    if x.shape[ROW_INDEX] != y.shape[ROW_INDEX]:
-        raise InvalidParamException('Numbers of rows in X must match number of rows in y')
+    if X.shape[ROW_INDEX] != y.shape[ROW_INDEX]:
+        raise IncompatibleDataShapeException(X.shape[0], y.shape[0])
 
 
 def convert_to_np_array(vector: any) -> ndarray:
