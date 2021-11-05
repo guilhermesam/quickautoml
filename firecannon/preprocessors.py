a76dd96fb0156ab6645d0803bf12ae7e3d6cdb00
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-11-05 09:14:54 -0300

    refact: rename things to domain context

--- firecannon/preprocessors.py
+++ firecannon/preprocessors.py
@@ -1,55 +1,58 @@
-from numpy import ndarray, array, amax
+from abc import ABC, abstractmethod
+
+from numpy import ndarray, array
 from pandas import DataFrame
 
 
-class Component:
+class AbstractStep(ABC):
+  @abstractmethod
   def run(self, matrix):
     pass
 
 
-class ConcreteComponent(Component):
+class ConcreteStep(AbstractStep):
   def run(self, matrix):
     return matrix
 
 
-class Decorator(Component):
-  def __init__(self, component: Component) -> None:
-    self._component = component
+class StepDecorator(AbstractStep):
+  def __init__(self, step: AbstractStep) -> None:
+    self._step = step
 
   @property
-  def component(self) -> Component:
-    return self._component
+  def step(self) -> AbstractStep:
+    return self._step
 
   def run(self, matrix):
-    return self._component.run(matrix)
+    return self._step.run(matrix)
 
 
-class GenericMatrixToDataframeDecorator(Decorator):
+class GenericMatrixToDataframeDecorator(StepDecorator):
   def run(self, matrix):
     matrix = DataFrame(matrix)
-    return self.component.run(matrix)
+    return self.step.run(matrix)
 
 
-class RemoveNullValuesDecorator(Decorator):
+class RemoveNullValuesDecorator(StepDecorator):
   def run(self, matrix):
     for col in matrix.columns:
       if matrix[col].isnull().all():
         matrix.drop(col)
     matrix.dropna(inplace=True)
-    return self.component.run(matrix)
+    return self.step.run(matrix)
 
 
-class RemoveDuplicatesDecorator(Decorator):
+class RemoveDuplicatesDecorator(StepDecorator):
   def run(self, matrix):
     matrix.drop_duplicates(inplace=True)
-    return self.component.run(matrix)
+    return self.step.run(matrix)
 
 
-class GenericMatrixToNPArrayDecorator(Decorator):
+class GenericMatrixToNPArrayDecorator(StepDecorator):
   def run(self, matrix):
     if not isinstance(matrix, ndarray):
       matrix = array(matrix)
-    return self.component.run(matrix)
+    return self.step.run(matrix)
 
 
 class DataPreprocessor:
@@ -57,13 +60,13 @@ class DataPreprocessor:
     pass
 
   @staticmethod
-  def __client_code(component: Component, matrix: any) -> None:
-    return component.run(matrix)
+  def __initialize_steps(step: AbstractStep, matrix: any) -> None:
+    return step.run(matrix)
 
   def run(self, matrix: any) -> object:
-    simple = ConcreteComponent()
-    decorator1 = GenericMatrixToNPArrayDecorator(simple)
-    decorator2 = RemoveDuplicatesDecorator(decorator1)
-    decorator3 = RemoveNullValuesDecorator(decorator2)
-    decorator4 = GenericMatrixToDataframeDecorator(decorator3)
-    return self.__client_code(decorator4, matrix)
+    first_step = ConcreteStep()
+    convert_matrix_to_array_step = GenericMatrixToNPArrayDecorator(first_step)
+    remove_duplicates_step = RemoveDuplicatesDecorator(convert_matrix_to_array_step)
+    remove_null_step = RemoveNullValuesDecorator(remove_duplicates_step)
+    convert_matrix_to_dataframe_step = GenericMatrixToDataframeDecorator(remove_null_step)
+    return self.__initialize_steps(convert_matrix_to_dataframe_step, matrix)
