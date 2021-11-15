91cf4ca35d4bdcb440f5847d1bc17322cbcc5fe2
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-11-14 21:41:26 -0300

    feat: dataset tests setup

--- tests/experiment/android_datasets.py
+++ tests/experiment/android_datasets.py
@@ -1,5 +1,6 @@
 from time import time
 from datetime import date, datetime
+from typing import TextIO
 
 from pandas import read_csv
 from sklearn.model_selection import train_test_split
@@ -7,42 +8,45 @@ from sklearn.metrics import accuracy_score
 
 from firecannon.estimators import Classifier
 
-file = open('results.txt', 'w')
+file = open('logs.txt', 'a')
 
-amn_start = time()
-drebin_df = read_csv('../datasets/android-malware-normal.csv')
-drebin_model = Classifier()
-X = drebin_df.drop('class', axis=1)
-y = drebin_df['class']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-drebin_model.fit(X_train, y_train)
-predicted = drebin_model.predict(X_test)
-amn_accuracy = accuracy_score(y_test, predicted)
-amn_end = time()
-print(f'Accuracy in Drebin-215: {amn_accuracy}')
-print(f'Drebin-215 execution time: {amn_end - amn_start}')
-print(drebin_df.shape, end='\n\n')
 
-androcrawl_start = time()
-defense_df = read_csv('../datasets/androcrawl.csv')
-defense_model = Classifier()
-X = defense_df.drop('class', axis=1)
-y = defense_df['class']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-defense_model.fit(X_train, y_train)
-predicted = defense_model.predict(X_test)
-androcrawl_accuracy = accuracy_score(y_test, predicted)
-androcrawl_end = time()
-print(f'Accuracy in Defense Droid: {androcrawl_accuracy}')
-print(f'Defense droid execution time: {androcrawl_end - androcrawl_start}')
-print(defense_df.shape)
+class TestSuite:
+  def __init__(self,
+               dataset_name: str,
+               dataset_path: str,
+               output_file: TextIO,
+               ):
+    self.output_file = output_file
+    self.dataset_name = dataset_name
+    self.dataset_path = dataset_path
+    self.estimator = Classifier()
 
-now = datetime.now()
-file.write(f'===== RUN => {date.today()} - {now.hour}:{now.minute}:{now.second}\n')
-file.write(f'Android Malware Normal accuracy: {amn_accuracy}\n')
-file.write(f'Android Malware Normal time: {amn_end - amn_start}\n')
-file.write(f'Androcrawl accuracy: {androcrawl_accuracy}\n')
-file.write(f'Androcrawl time: {androcrawl_end - androcrawl_start}\n')
-# file.write('Comments\n')
-file.write(f'==============================================\n')
-file.close()
+  def run(self, y_label: str = 'class'):
+    start = time()
+    df = read_csv(self.dataset_path)
+    X = df.drop(y_label, axis=1)
+    y = df[y_label]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    self.estimator.fit(X_train, y_train)
+    predicted = self.estimator.predict(X_test)
+    accuracy = accuracy_score(y_test, predicted)
+    end = time()
+    now = datetime.now()
+    self.output_file.write(f'===== RUN => {date.today()} - {now.hour}:{now.minute}:{now.second}\n')
+    self.output_file.write(f'{self.dataset_name} accuracy: {accuracy}\n')
+    self.output_file.write(f'{self.dataset_name} time: {end - start}\n')
+    self.output_file.write(f'==============================================\n')
+
+  def close(self):
+    self.output_file.close()
+
+
+androcrawl = TestSuite(
+  dataset_name='Androcrawl',
+  dataset_path='../datasets/androcrawl.csv',
+  output_file=file
+)
+
+androcrawl.run()
+androcrawl.close()
