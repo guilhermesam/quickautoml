7f05d702ab2862c582c11c9eb04baac747caa695
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-12-01 20:48:33 -0300

    chore: add coverage lib

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as docs:
-    long_description = docs.read()
-
 setup(
     name='firecannon',
     version='0.0.1',
     author='Wabbajack',
     packages=find_packages(include=['firecannon']),
     description='Quick tests for supervised machine learning entities',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     license='MIT',
     install_requires=[
-        'sklearn', 'numpy', 'pandas', 'matplotlib'
+        'sklearn', 'numpy', 'pandas', 'matplotlib', 'optuna'
     ],
     test_suite='tests'
 )
