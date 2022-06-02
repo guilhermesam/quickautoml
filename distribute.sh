a936b4a893769e09435e0d4d94eaf2028148ddce
Guilherme Samuel <guilherme.siqueira@combateafraude.com>
2022-06-02 18:27:24 -0300

    fix: update distribute command to install with python3

--- distribute.sh
+++ distribute.sh
@@ -2,5 +2,5 @@ echo "Installing required modules"
 pip install wheel
 pip install setuptools
 
-python setup.py bdist_wheel
+python3 setup.py bdist_wheel
 pip install dist/quickautoml-1.0.0-py3-none-any.whl
