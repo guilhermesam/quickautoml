51c4cfcb84a1f8bd640b390169021d805b753854
Guilherme Samuel <guilherme.siqueira@caf.io>
2022-12-10 15:36:15 -0300

    chore: reformat files to PEP8

--- quickautoml/adapters.py
+++ quickautoml/adapters.py
@@ -13,12 +13,12 @@ from quickautoml.protocols import Metrics
 
 
 class ModelsSupplier(ABC):
-  def __init__(self) -> None:
-    self._models_codes = None
+    def __init__(self) -> None:
+        self._models_codes = None
 
-  @abstractmethod
-  def get_model(self, model_name: str):
-    """
+    @abstractmethod
+    def get_model(self, model_name: str):
+        """
     Deve retornar uma instância de um modelo, de acordo com o nome do mesmo. Possíveis nomes:
     Classificadores:
     knn-c: KNearestNeighbors Classifier
@@ -30,44 +30,44 @@ class ModelsSupplier(ABC):
     lasso: Lasso Regressor
     en: ElasticNet Regressor
     """
-    pass
+        pass
 
 
 class SKLearnModelsSupplier(ModelsSupplier):
-  def __init__(self) -> None:
-    super().__init__()
-    self._models_codes = {
-      'knn-c': KNeighborsClassifier(),
-      'rf-c': RandomForestClassifier(),
-      'ada-c': AdaBoostClassifier(),
-      'svc': SVC(),
-      'dt': DecisionTreeClassifier(),
-      'rf-r': RandomForestRegressor(),
-      'lasso': Lasso(),
-      'en': ElasticNet()
-    }
+    def __init__(self) -> None:
+        super().__init__()
+        self._models_codes = {
+            'knn-c': KNeighborsClassifier(),
+            'rf-c': RandomForestClassifier(),
+            'ada-c': AdaBoostClassifier(),
+            'svc': SVC(),
+            'dt': DecisionTreeClassifier(),
+            'rf-r': RandomForestRegressor(),
+            'lasso': Lasso(),
+            'en': ElasticNet()
+        }
 
-  def get_model(self, model_name: str) -> object:
-    return self._models_codes.get(model_name)
+    def get_model(self, model_name: str) -> object:
+        return self._models_codes.get(model_name)
 
 
 class SKLearnMetrics(Metrics):
-  @staticmethod
-  def mse(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
-    return mean_squared_error(y_true, y_pred)
+    @staticmethod
+    def mse(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
+        return mean_squared_error(y_true, y_pred)
 
-  @staticmethod
-  def r2(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
-    return r2_score(y_true, y_pred)
+    @staticmethod
+    def r2(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
+        return r2_score(y_true, y_pred)
 
-  @staticmethod
-  def accuracy(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
-    return accuracy_score(y_true, y_pred)
+    @staticmethod
+    def accuracy(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
+        return accuracy_score(y_true, y_pred)
 
-  @staticmethod
-  def precision(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
-    return precision_score(y_true, y_pred)
+    @staticmethod
+    def precision(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
+        return precision_score(y_true, y_pred)
 
-  @staticmethod
-  def recall(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
-    return recall_score(y_true, y_pred)
+    @staticmethod
+    def recall(y_true: Union[ndarray, List[float]], y_pred: Union[ndarray, List[float]]):
+        return recall_score(y_true, y_pred)
--- quickautoml/exceptions.py
+++ quickautoml/exceptions.py
@@ -1,25 +1,25 @@
 class IncompatibleDataShapeException(Exception):
-  def __init__(self, x_rows: int, y_rows: int):
-    self.message = f'X has {x_rows} rows, but y has {y_rows} rows'
-    super(Exception, self).__init__(self.message)
+    def __init__(self, x_rows: int, y_rows: int):
+        self.message = f'X has {x_rows} rows, but y has {y_rows} rows'
+        super(Exception, self).__init__(self.message)
 
-  def __str__(self):
-    return self.message
+    def __str__(self):
+        return self.message
 
 
 class ModelNotFittedException(Exception):
-  def __init__(self, message: str) -> None:
-    super().__init__(message)
-    self.message = message
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
 
-  def __str__(self) -> str:
-    return self.message
+    def __str__(self) -> str:
+        return self.message
 
 
 class InvalidParamException(Exception):
-  def __init__(self, message: str) -> None:
-    super().__init__(message)
-    self.message = message
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
 
-  def __str__(self) -> str:
-    return self.message
+    def __str__(self) -> str:
+        return self.message
